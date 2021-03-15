78c1b00a13cc463e25495e6250fb7048ca4d1132
Replit user <replituser@example.com>
2021-03-15 18:32:43 +0000

    new try

--- exercises.c
+++ exercises.c
@@ -122,7 +122,7 @@ la cual retorna el valor en la posición i del vector v.
 */
 int obtenerValor(Vector * v, int i) 
 {
-  int temporal = *v[i - 1].datos;
+  int temporal = *v[2 - i].datos;
   printf(" %i %i \n", temporal, i);
 
   return *(v[i].datos);
