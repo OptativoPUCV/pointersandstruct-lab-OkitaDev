07a99965dbb82647f5bed050cbc35bdb6aacc47b
Replit user <replituser@example.com>
2021-03-15 03:35:24 +0000

    new try

--- exercises.c
+++ exercises.c
@@ -67,13 +67,7 @@ typedef struct {
 
 Persona* crearPersona(char nombre[], char rut[], int edad) 
 {
-   Persona * persona = NULL;
-   
-   strcpy(persona->nombre, nombre);
-   strcpy(persona->rut, rut);
-   persona->edad = edad;
-
-   return persona;
+   return NULL;
 }
 
 /*
@@ -126,6 +120,7 @@ Ejercicio 8.
 Use las operaciones implementadas de vectores para 
 sumar (a1,a2)+(b1+b2). Almacene el resultado en el vector c.
 */
-void sumaV2(int a1, int a2, int b1, int b2, Vector *c){
+void sumaV2(int a1, int a2, int b1, int b2, Vector *c)
+{
 
 }
