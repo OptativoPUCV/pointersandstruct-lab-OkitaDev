5ac7ed6efe12ec401d78ec1acbc8d5e942647e5d
Replit user <replituser@example.com>
2021-03-16 22:53:57 +0000

    new try

--- exercises.c
+++ exercises.c
@@ -111,7 +111,7 @@ la cual asigna el valor a la posición i del vector v.
 void asignarValor(Vector * v, int i, int valor) 
 {
 	int * temporal = &valor;
-	v->datos = temporal;
+	v[i].datos = temporal;
 }
 
 /*
